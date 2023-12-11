9565dd6a20d557ca5ef7ac56792ff6c7b982da2c
CabosManuel <cabos.apolaya@gmail.com>
2023-12-11 18:33:54 -0500

    First Hook "useStatus()" and first Status

--- src/components/TodoNew.js
+++ src/components/TodoNew.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
+
 function TodoNew() {
+  // React state (immutable)
+  // [stateName, setStateFunctionName]
+  const [newTaskValue, setNewTaskValue] = useState(''); // '' Initial value
+  console.log('newTaskValue', newTaskValue);
+
   return (
     <input
       type="text"
       className="new-todo__text"
       placeholder="Add + new task..."
+      value={newTaskValue} // <-- using state to show value
       onChange={
         (event) => {
-          console.log('Typing todo...');
-          console.log(event.target.value);
+          // console.log('Typing todo...');
+          // console.log(event.target.value);
+          setNewTaskValue(event.target.value);
         }
       }
     />
