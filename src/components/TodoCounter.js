acbe4c237fc0dab08f4163fa33370f260d7c5de4
CabosManuel <cabos.apolaya@gmail.com>
2023-12-11 11:55:44 -0500

    Update TodoCounter & add styles for btn and counter

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,8 +1,14 @@
+import { FaListCheck } from "react-icons/fa6";
+import { FaCircleCheck } from "react-icons/fa6";
+import { FaRegCircleCheck } from "react-icons/fa6";
+
 function TodoCounter({ total, completed }) {
   return (
-    <h1>
-      Has completado {completed} de {total} TODOs
-    </h1>
+    <div className="todo-counter">
+      <span className="todo-counter__total"> <FaListCheck/> TOTAL: {total} </span>
+      <span className="todo-counter__todo"> <FaRegCircleCheck/> TODO: {total - completed} </span>
+      <span className="todo-counter__completed"> <FaCircleCheck/> COMPLETED: {completed} </span>
+    </div>
   );
 }
 
