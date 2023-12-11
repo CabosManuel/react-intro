57171caa3f0e0b8cb002e04dced233543d229842
CabosManuel <cabos.apolaya@gmail.com>
2023-12-11 13:18:02 -0500

    Add styles for delete btn

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,15 @@
+import { FaTrash } from "react-icons/fa6";
+
 function TodoItem(props) {
   console.log(props);
   return (
     <li className="todos__item">
       <input
-        className=""
         type="checkbox"
         // checked={props.completed}
       />
-      <label className="">{props.title}</label>
-      
+      <label className="todos__item__task">{props.title}</label>
+      <FaTrash className="todo__item__delete"/>
     </li>
   );
 }
