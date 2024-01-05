d78391333bbbd132e7d719f6bb86ff9938fbd8ed
CabosManuel <cabos.apolaya@gmail.com>
2024-01-05 13:32:34 -0500

    Implement React Context

--- src/App.js
+++ src/App.js
@@ -11,107 +11,74 @@ import { CreateTodoBtn } from './components/CreateTodo/CreateTodoBtn';
 import { TodoSearch } from './components/Sidebar/TodoSearch';
 import { SidebarItem } from './components/Sidebar/SidebarItem';
 import { FaClipboardCheck } from "react-icons/fa6";
-import { useLocalStorage } from './utils/useLocalStorage';
+import { TodoContext, TodoProvider } from './components/TodoContext';
 
 function App() {
-  // Usando el Hook "useState" para crear un estado y su modificador
-  // y poder utilizarlo para escribir en el buscador
-  //
-  // Desestructurando state en [ value, setValue ]
-  const [searchValue, setSearchValue] = useState('');
-  const {
-    // Renombrar 'item' -> 'todos'
-    item: todos,
-    updateItem: updateTodos,
-    loading,
-    error
-  } = useLocalStorage('TODOS', []);
-  // TODO: Ver como enviar un initialValue [], que sea un array con unas tareas por defecto,
-  // que no de error y que si pueda marcar la tarea completada o eliminada
-
-  // Estados derivados:
-  // Variables / propiedades / cálculos que vienen a partir de un estado
-  const completedTodos = todos.filter((todo) => !!todo.completed).length;
-  const totalTodos = todos.length;
-
-  const searchedTodo = todos.filter(
-    (todo) => {
-      const todoTitle = todo.title.toLocaleUpperCase();
-      const searchText = searchValue.toLocaleUpperCase();
-      return todoTitle.includes(searchText);
-    }
-  );
-
-  // Función para actualizar estado de las tareas cuando cambia un checkbox
-  const checkTodo = (index) => {
-    const newTodos = [...todos];
-    const isCompleted = newTodos[index].completed;
-    newTodos[index].completed = (isCompleted) ? false : true;
-    updateTodos(newTodos);
-  }
-  
-  // Función para eliminar todo cuando hace click al ícono
-  const deleteTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    updateTodos(newTodos);
-  }
-
   return (
-    <div className='main'>
-    <div className='content'>
-      <h1>
-        <FaClipboardCheck/>
-        ToDo App
-      </h1>
-      <div className='new-todo'>
-        <TodoNew/>
-        <CreateTodoBtn/>
+    // Encapsular todo dentro del Provider para que todos sean children
+    <TodoProvider>
+      <div className='main'>
+        <div className='content'>
+          <h1>
+            <FaClipboardCheck/>
+            ToDo App
+          </h1>
+          <div className='new-todo'>
+            <TodoNew/>
+            <CreateTodoBtn/>
+          </div>
+          <TodoCounter />
+
+          {/* Encapsular dentro de Consumer específicamente los componentes que necesitan las props*/}
+          <TodoContext.Consumer>
+            {/*
+              Se crear un arrow function con las props específicas y retorna el componente encapsulado:
+              {({props}) => (component)}
+            */}
+            {({loading, error, searchedTodo, checkTodo, deleteTodo}) => (
+              <TodoList>
+                { // Si loading es true, mostrar msj
+                  (loading && searchedTodo.length === 0)
+                    ? <TodosLoading />
+                    : null
+                }
+                { // Si error es true, mostrar msj
+                  (error)
+                    ? <TodosError />
+                    : null
+                }
+                { // Si loading es true y la cantidad del array es 0, mostrar msj
+                  (!loading && searchedTodo.length < 0)
+                    ? <TodosEmpty />
+                    : null
+                }
+
+                { searchedTodo.map((todo, index) => (
+                  <TodoItem
+                    // Enviar props: key, title, completed, onDelete, onCheck
+                    key={todo.id}
+                    title={todo.title}
+                    completed={todo.completed}
+                    onDelete={deleteTodo}
+                    // 💡 React solo recibe funciones sin parámetros, así que se encapsula
+                    // en una arrow function para poder enviar un parámetro dentro
+                    onCheck={() => {
+                      checkTodo(index);
+                    }}
+                  />
+                )) }
+              </TodoList>
+            )}
+          </TodoContext.Consumer>
+
+        </div>
+        <div className='sidebar'>
+          <SidebarItem>
+            <TodoSearch />
+          </SidebarItem>
+        </div>
       </div>
-      <TodoCounter total={totalTodos} completed={completedTodos}/>
-
-      <TodoList>
-        { // Si loading es true, mostrar msj
-          (loading && searchedTodo.length === 0)
-            ? <TodosLoading />
-            : null
-        }
-        { // Si error es true, mostrar msj
-          (error)
-            ? <TodosError />
-            : null
-        }
-        { // Si loading es true y la cantidad del array es 0, mostrar msj
-          (!loading && searchedTodo.length < 0)
-            ? <TodosEmpty />
-            : null
-        }
-
-        { searchedTodo.map((todo, index) => (
-          <TodoItem
-            // Enviar props: key, title, completed, onDelete, onCheck
-            key={todo.id}
-            title={todo.title}
-            completed={todo.completed}
-            onDelete={deleteTodo}
-            // 💡 React solo recibe funciones sin parámetros, así que se encapsula
-            // en una arrow function para poder enviar un parámetro dentro
-            onCheck={() => {
-              checkTodo(index);
-            }}
-          />
-        )) }
-      </TodoList>
-    </div>
-    <div className='sidebar'>
-      <SidebarItem>
-        <TodoSearch
-          searchValue={searchValue}
-          setSearchValue={setSearchValue}
-        />
-      </SidebarItem>
-    </div>
-    </div>
+    </TodoProvider>
   );
 }
 
---
+++ src/components/TodoContext.js
@@ -0,0 +1,74 @@
+import { useState, createContext } from "react";
+import { useLocalStorage } from "../utils/useLocalStorage";
+
+// Crear un context
+const TodoContext = createContext();
+
+function TodoProvider({ children }) {
+  /* ************************** Traer toda la lógica del App() al Context ************************** */
+
+  // Usando el Hook "useState" para crear un estado y su modificador
+  // y poder utilizarlo para escribir en el buscador
+  //
+  // Desestructurando state en [ value, setValue ]
+  const [searchValue, setSearchValue] = useState('');
+  const {
+    // Renombrar 'item' -> 'todos'
+    item: todos,
+    updateItem: updateTodos,
+    loading,
+    error
+  } = useLocalStorage('TODOS', []);
+  // TODO: Ver como enviar un initialValue [], que sea un array con unas tareas por defecto,
+  // que no de error y que si pueda marcar la tarea completada o eliminada
+
+  // Estados derivados:
+  // Variables / propiedades / cálculos que vienen a partir de un estado
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  const searchedTodo = todos.filter(
+    (todo) => {
+      const todoTitle = todo.title.toLocaleUpperCase();
+      const searchText = searchValue.toLocaleUpperCase();
+      return todoTitle.includes(searchText);
+    }
+  );
+
+  // Función para actualizar estado de las tareas cuando cambia un checkbox
+  const checkTodo = (index) => {
+    const newTodos = [...todos];
+    const isCompleted = newTodos[index].completed;
+    newTodos[index].completed = (isCompleted) ? false : true;
+    updateTodos(newTodos);
+  }
+  
+  // Función para eliminar todo cuando hace click al ícono
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    updateTodos(newTodos);
+  }
+
+  /* ************************ END Traer toda la lógica del App() al Context ************************ */
+
+  return (
+    // value:
+    // Almacena en forma de obj todas las props a las que podrán acceder todos los children
+    <TodoContext.Provider value={{
+      loading,
+      error,
+      completedTodos,
+      totalTodos,
+      searchValue,
+      setSearchValue,
+      searchedTodo,
+      checkTodo,
+      deleteTodo
+    }}>
+      {children}
+    </TodoContext.Provider>
+  );
+}
+
+export { TodoContext, TodoProvider };
